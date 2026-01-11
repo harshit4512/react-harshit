720fe53c5888d45f9364022015e7a75189e539d6
Harshit Purwar <harshit.purwar2004@gmail.com>
2026-01-11 23:50:24 +0530

    learning components

---
+++ 02-components/src/components/card.jsx
@@ -0,0 +1,9 @@
+function Card(){
+    return ( 
+    <div className="card">
+        <h1 >i am harshit</h1>
+    </div>
+    )
+}
+
+export default Card
---
+++ 02-components/src/components/navbar.jsx
@@ -0,0 +1,9 @@
+function Navbar(){
+    return (
+        <div className="nav">
+            <h3>this is a navbar</h3>
+        </div>
+    )
+}
+
+export default Navbar
